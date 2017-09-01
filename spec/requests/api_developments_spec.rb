ab13e801a7ee8d7d5e39296608dda94111e2a4c1
Shabbir Haider <syed_shabbir_haider@hotmail.com>
2017-09-01 00:58:23 +0000

    added mongoid configs, configured routes and tests

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
-  describe "GET /api_developments" do
-    it "works! (now write some real specs)" do
-      get api_developments_path
-      expect(response).to have_http_status(200)
+  def parsed_body
+    JSON.parse(response.body) if response.body && response.body.length >=2
+  end
+  describe "MongoDB backed API resource" do
+    before(:each) {Bar.delete_all}
+    after(:each) {Bar.delete_all}
+    
+    it 'creates MongoDB-backed resource' do
+      object = Bar.create(name: 'test')
+      expect(Bar.find(object.id).name).to eq('test')
     end
+
+    it 'exposes MongoDB-backed API resource' do
+      object = Bar.create(name: 'test')
+      expect(bars_path).to eq('/api/bars')
+      get bar_path(object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body['name']).to eq('test')
+      #expect(parsed_body).to include('created_at')
+      #expect(parsed_body).to include('id'=> object.id.to_s)
+    end
+    
   end
 end
